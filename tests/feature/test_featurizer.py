c43c443288b6e576bb266cff26d6079f80a4da8d
Carsten Schnober <c.schnober@esciencecenter.nl>
2023-04-05 17:13:21 +0200

    Move featurizer test fixture to conftest.py.

--- tests/feature/test_featurizer.py
+++ tests/feature/test_featurizer.py
@@ -1,29 +1,8 @@
 import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
-from text_quality.feature.featurize import Featurizer
 from text_quality.feature.featurize import Scorers
-from text_quality.feature.scorer.dictionary import HunspellDictionary
-from text_quality.feature.scorer.dictionary import TokenDictionary
-from text_quality.feature.scorer.garbage import GarbageDetector
-from text_quality.feature.scorer.q_gram import QGram
-from text_quality.feature.tokenizer import NautilusOcrTokenizer
-from text_quality.settings import HUNSPELL_DIR
-from text_quality.settings import HUNSPELL_LANGUAGE
-from text_quality.settings import QGRAMS_FILE
-from text_quality.settings import TOKEN_DICT_FILE
-
-
-@pytest.fixture
-def featurizer():
-    scorers = Scorers(
-        dict_score=HunspellDictionary.from_path(HUNSPELL_DIR, HUNSPELL_LANGUAGE),
-        dict_score_gt=TokenDictionary.from_file(TOKEN_DICT_FILE),
-        n_gram_score=QGram.from_file(QGRAMS_FILE),
-        garbage_score=GarbageDetector(),
-    )
-    tokenizer = NautilusOcrTokenizer()
-    return Featurizer(scorers, tokenizer)
+from ..conftest import featurizer
 
 
 class TestFeaturizer:
