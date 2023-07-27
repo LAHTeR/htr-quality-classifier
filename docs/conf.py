16780c2b4888c72be046d972ef12cb30f130b26d
Carsten Schnober <c.schnober@esciencecenter.nl>
2023-07-27 16:10:54 +0200

    Bump version: 0.1.5 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u"Carsten Schnober"
 # built documents.
 #
 # The short X.Y version.
-version = "0.1.5"
+version = "0.2.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
